5cf7e1bfae740d542453c8043c4ca1f993459938
jayprakash6547 <prabalmangal@gmail.com>
2026-08-14 11:41:26 +0530

    dynamic button through setState

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import Greet from './component/Greet';
 import Hello from './component/Hello';
+import Message from './component/Message';
 import Welcome from './component/Welcome';
 
 function App() {
   return (
     <div className="App">
+
+
+      <Message/>
   {/* <Greet name="Alice" designation="Engineer"/>
   <Greet name="Bob" designation="Designer"/>
   <Greet name="Charlie" designation="Manager"/> */}
-  <Welcome name="David" designation="Engineer"/>
+  {/* <Welcome name="David" designation="Engineer"/>
   <Welcome name="Eve" designation="Designer"/>
-  <Welcome name="Frank" designation="Manager"/>
+  <Welcome name="Frank" designation="Manager"/> */}
   {/* <Hello/> */}
     </div>
   );
