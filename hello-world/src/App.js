2ecd3bbd2967dbd1deb0fe02e964dcebe9016f25
jayprakash6547 <prabalmangal@gmail.com>
2026-08-14 07:07:33 +0530

    latest

---
+++ hello-world/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Greet from './component/Greet';
+import Welcome from './component/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+  <Greet/>
+  <Welcome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-world/src/component/Greet.js
@@ -0,0 +1,9 @@
+import react from 'react';
+
+// function Greet(){
+//     return <h1>Hello JayPrakash</h1>
+// }
+const Greet = () => <h1>Hello JayPrakash</h1>
+
+
+export default Greet;
