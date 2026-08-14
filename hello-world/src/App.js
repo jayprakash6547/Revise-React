676be3078e1188bbd9ee9eb7721157e80f2df276
jayprakash6547 <prabalmangal@gmail.com>
2026-08-14 11:29:37 +0530

    propscomplete

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -6,10 +6,12 @@ import Welcome from './component/Welcome';
 function App() {
   return (
     <div className="App">
-  <Greet name="Alice" designation="Engineer"/>
+  {/* <Greet name="Alice" designation="Engineer"/>
   <Greet name="Bob" designation="Designer"/>
-  <Greet name="Charlie" designation="Manager"/>
-  {/* <Welcome/> */}
+  <Greet name="Charlie" designation="Manager"/> */}
+  <Welcome name="David" designation="Engineer"/>
+  <Welcome name="Eve" designation="Designer"/>
+  <Welcome name="Frank" designation="Manager"/>
   {/* <Hello/> */}
     </div>
   );
--- hello-world/src/component/Welcome.js
+++ hello-world/src/component/Welcome.js
@@ -3,7 +3,7 @@ import React,{Component} from 'react';
 class Welcome extends
 Component{
     render(){
-        return <h1>Hello class Component JayPrakash</h1>
+        return <h1>Hello class Component {this.props.name} a.k.a {this.props.designation}</h1>
     }
 }
 export default Welcome;
