aa66d59060b9c22544a4a16dc80433b8a6e16bfb
jayprakash6547 <prabalmangal@gmail.com>
2026-08-15 14:39:04 +0530

    List Rendering

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,52 +1,35 @@
-import './App.css';
-import Greet from './component/Greet';
-import Hello from './component/Hello';
-import Message from './component/Message';
-import Welcome from './component/Welcome';
-import Counter from './component/Counter';
-import FunctionClick from './component/FunctionClick';
-import ClassClick from './component/ClassClick';
-import EventBind from './component/EventBind';
-import ParentComponent from './component/ParentComponent';
+import "./App.css";
+import Greet from "./component/Greet";
+import Hello from "./component/Hello";
+import Message from "./component/Message";
+import Welcome from "./component/Welcome";
+import Counter from "./component/Counter";
+import FunctionClick from "./component/FunctionClick";
+import ClassClick from "./component/ClassClick";
+import EventBind from "./component/EventBind";
+import ParentComponent from "./component/ParentComponent";
+import NameList from "./component/NameList";
 
 function App() {
   return (
     <div className="App">
-      <ParentComponent/>
+      <NameList />
+      {/* <ParentComponent /> */}
 
-    {/* <EventBind/> */}
-
-
+      {/* <EventBind/> */}
 
       {/* <FunctionClick/>
       <ClassClick /> */}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <Counter/> */}
+      {/* <Counter/> */}
       {/* <Message/> */}
       {/* <Greet name="Alice" designation="Developer"/> */}
-  {/* <Greet name="Bob" designation="Designer"/>
+      {/* <Greet name="Bob" designation="Designer"/>
   <Greet name="Charlie" designation="Manager"/>  */}
-  {/* <Welcome name="David" designation="Engineer"/>
+      {/* <Welcome name="David" designation="Engineer"/>
   <Welcome name="Eve" designation="Designer"/>
   <Welcome name="Frank" designation="Manager"/> */}
-  {/* <Hello/> */}
+      {/* <Hello/> */}
     </div>
   );
 }
