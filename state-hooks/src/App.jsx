bc355ebe7b45ce86dc5936b028c5deaf456fd9f1
jayprakash6547 <prabalmangal@gmail.com>
2026-08-20 07:49:18 +0530

    useState with previous state

--- state-hooks/src/App.jsx
+++ state-hooks/src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react'
 import './App.css'
 import ClassCounter from './Components/ClassCounter'
 import HookCounter from './Components/HookCounter'
+import HookCounterTwo from './HookCounterTwo'
 
 function App() {
   
 
   return (
     <>
+    <HookCounterTwo />
     {/* <ClassCounter />
     <HookCounter /> */}
     </>
